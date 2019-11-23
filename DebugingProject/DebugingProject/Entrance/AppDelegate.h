abe1ea3667ec7765037dbeff7cf2b845b693b4f0
ShenYj <shenyj4@51nb.com>
2019-11-23 22:23:03 +0800

    init project

---
+++ DebugingProject/DebugingProject/Entrance/AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.h
+//  DebugingProject
+//
+//  Created by ShenYj on 2019/11/23.
+//  Copyright © 2019 ShenYj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
