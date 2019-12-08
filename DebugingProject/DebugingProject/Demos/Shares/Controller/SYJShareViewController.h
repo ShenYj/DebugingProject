bf61a8dead1fe8f9fcdcaf010dbd939a13f1e3b7
ShenYj <shenyj4@51nb.com>
2019-12-08 21:15:04 +0800

    分享 demo

---
+++ DebugingProject/DebugingProject/Demos/Shares/Controller/SYJShareViewController.h
@@ -0,0 +1,17 @@
+//
+//  SYJShareViewController.h
+//  DebugingProject
+//
+//  Created by ShenYj on 2019/12/8.
+//  Copyright © 2019 ShenYj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SYJShareViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ DebugingProject/DebugingProject/Demos/Shares/LWShareView/LWShareContentView.h
@@ -0,0 +1,22 @@
+//
+//  ArtShareContentView.h
+//  LWShareView
+//
+//  Created by LeeWong on 2018/1/9.
+//  Copyright © 2018年 LeeWong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+UIKIT_EXTERN NSString * const kShareIcon;
+UIKIT_EXTERN NSString * const kShareTitle;
+UIKIT_EXTERN NSString * const kShareType;
+
+@interface LWShareContentView : UIView
+
+//防止按钮的数组 
+@property (nonatomic, strong) NSArray *topMenus;
+@property (nonatomic, strong) NSArray *bottomMenus;
+@property (nonatomic, copy) void (^shareBtnClickBlock)(NSIndexPath *index);
+
+@end
